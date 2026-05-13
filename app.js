bc7fa0cb6c368caf3c98897cf17035e7eccf3f52
John Walter Munene Njeru <midwaymaster10@gmail.com>
2026-05-13 17:33:17 +0300

    On setting up project dependencies, and designing data models.

---
+++ app.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const app = express();
+const path = require("node:path");
+const pool = require("./db/pool");
+const authRouter = require("./routes/authRouter");
+
+// Templating engine setup.
+app.set("views", path.join(__dirname, "views"));
+app.set("view-engine", "ejs");
+
+// Serving static assets.
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// Routes.
+app.get("/", (request, response) => {
+  response.send("Building a members only application");
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    store: new pgSession({
+      pool: pool,
+      tableName: "users_sessions",
+    }),
+    secret: "cats",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+// Passport setup.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Make the current user available in all views.
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// Routes.
+app.use(authRouter);
+
+// Error Handling
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.status(error.statusCode || 500).render("404");
+});
+
+const PORT = Number(process.env.PORT) || 3000;
+
+// Start the server.
+app.listen(PORT, (error) => {
+    if (error) throw new Error(error);
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/authRouter.js
@@ -0,0 +1,13 @@
+const passport = require("passport");
+const { Router } = require("express");
+const authController = require("../controllers/authController");
+const authRouter = Router();
+
+authRouter.get("/sign-up", authController.getSignUp);
+authRouter.post("/sign-up", authController.postSignUp);
+
+authRouter.get("/sign-in", authController.getSignIn);
+authRouter.post("/sign-in", passport.authenticate("local"), { successRedirect: "/", failureRedirect: "/sign-in", });
+authRouter.get("/sign-out", authController.signOut);
+
+module.exports = authRouter;
